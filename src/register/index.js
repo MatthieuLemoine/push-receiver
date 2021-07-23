ca873ea24d78f17351bbae2bd7b77451d6ba5113
Patrick Remy <github@patrick-remy.dev>
2021-07-23 09:21:53 +0200

    :hammer: Add bundleId option on register

--- src/register/index.js
+++ src/register/index.js
@@ -4,9 +4,9 @@ const registerFCM = require('../fcm');
 
 module.exports = register;
 
-async function register(senderId) {
+async function register(senderId, { bundleId } = { bundleId : 'receiver.push.com' }) {
   // Should be unique by app - One GCM registration/token by app/appId
-  const appId = `wp:receiver.push.com#${uuidv4()}`;
+  const appId = `wp:${bundleId}#${uuidv4()}`;
   const subscription = await registerGCM(appId);
   const result = await registerFCM({
     token : subscription.token,
